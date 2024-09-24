1f75cecfdfed29ff3ca8b89fa184ae44010c40fa
waeeelhajji <waelhajji.contact@gmail.com>
2024-09-24 08:10:50 +0100

    Week1Day1

--- Week1Day1/Variables-Data-Types.js
+++ Week1Day1/Variables-Data-Types.js
@@ -1,11 +1,73 @@
+// What is JS  and why we need to learn It ?
 
 
 
-// String 
+// Variables and Data Types 
+
+// What is a variable ?
+
+// Stores data or information
+
 var name = "Alice"
-var number = "7845"
+// Variables names should have some "common sense" for this Box Or Container*
+var firstName = "Wael"
 
-var  theRealNumber = 85
+// JS we use camelCase - first word is lowerCase and  all the Rest are Capital Case( the first Later)
+
+//=======Data type (String)========
+
+var programmingBasicsStudents = "Dojo";
+
+var isThisValidVariableName = "Yes or No ?"
+
+/*
+programmingBasicsStudents = "Another NAme"
+
+programmingBasicsStudents = isThisValidVariableName; 
+
+var MAN_I_REALLY_LIKE_PANCAKES_AND_TACOS ="keck yea"
+
+var lsdjflkdsjfljdskljscljglkgjfdlkjglkfjgfkdlj = "LoL"
+*/
+console.log(name) // "Alice"
+
+
+
+//=======Data type (Number)========
+
+// 12 * Number/integer
+
+var number = 7777
+var pi = 3.14
+console.log(number) // 77777
+
+var THIS_IS_Weird = "1990"; // String 
+
+var num1 = 5
+var num2 = "10"
+num1 + num2 // 15 == 510
+
+
+
+//=======Data type (Boolean)========
+
+// CTRL + /
+// var vote1 = true
+var vote2 = false
+
+
+//=======Data type (undefined)========
+// ¯\_(ツ)_/¯
+// Variable with no assigned value
+var cool; // undefined
+
+//=======Data type (NULL)========
+// data that is not undefined, but  empty. 
+var initialState = null;
+var x = ""
+
+
+
 
 
 
