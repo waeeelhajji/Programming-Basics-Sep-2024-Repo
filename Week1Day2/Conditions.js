009d9c0484bacf98c670b1fb3ac700d1049dd84d
waeeelhajji <waelhajji.contact@gmail.com>
2024-09-25 12:52:51 +0100

    Week1Day3

--- Week1Day2/Conditions.js
+++ Week1Day2/Conditions.js
@@ -1,3 +1,11 @@
+// Conditions like they are checkpoints in video game + 18
+
+
+// if statements
+
+// if statements is the simplest way and form of conditions. you provide it with a condition (the equivalant of "Do I have the Key item?"), and it checks if that condition is true. if it is, the code inside the if statment runs.
+
+
 
 // if Statment 
 if (condition) { // True
---
+++ Week1Day3/FunctionAndParameters.js
@@ -0,0 +1,40 @@
+var animals = ['Red Panda', 'Elephant', 'Koala', 'Bear'];
+var fruits = ["🍉", "🍊", "🍌", "🍍", "🍑", "🍒", "🍈", "🍇", "🐾"]
+var number = [33, 55, 77, -22, 789, 1000, 22]
+
+// f -- name -- parameter
+function great(){
+    // Logic
+
+    return "Hello"
+// for()
+}
+// Function is Equal ro ITS Return 
+// call me the fuction // invoke the fuction 
+var fun = great // great()
+
+console.log(fun) // 8 HEllo 
+
+
+// --------------------
+
+function great(){
+    // Logic
+
+    return "Hello " + person
+// for()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
